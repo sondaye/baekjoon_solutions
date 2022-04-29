d9e9b21a0d0c885e575a1e67a61b691bb08e2842
sondayae\sonda <sondayae@gmail.com>
2022-04-30 00:07:35 +0900

    백준 step1 2557번, 10171번

---
+++ baekjoon/src/baekjoon_step1/prob_10171.java
@@ -0,0 +1,12 @@
+package baekjoon_step1;
+
+public class prob_10171 {
+	    public static void main(String[] args) {
+	        System.out.println("\\    /\\");
+			System.out.println(" )  ( ')");
+			System.out.println("(  /  )");
+			System.out.println(" \\(__)|");
+		
+		}
+	}
+		// TODO Auto-generated method stub
